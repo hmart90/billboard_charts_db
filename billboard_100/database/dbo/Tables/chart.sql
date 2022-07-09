515fa4ca453044e7240a397f6cfb6b0b3f9ccdf5
Martina Halász <h.martinaa@gmail.com>
2022-07-09 14:34:20 +0200

    first working version

---
+++ billboard_100/database/dbo/Tables/chart.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[chart]
+(
+	[chart_id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [date]           DATE          NOT NULL,
+    [rank]           INT           NOT NULL,
+    [song]           VARCHAR (500) NOT NULL,
+    [artist]         VARCHAR (500) NOT NULL,
+    [last-week]      INT           NULL,
+    [peak-rank]      INT           NOT NULL,
+    [weeks-on-board] INT           NOT NULL 
+)
---
+++ billboard_100/database/dbo/Tables/chart_staging.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[chart_staging]
+(
+	[chart_staging_id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [date]           VARCHAR (500) NULL,
+    [rank]           VARCHAR (500) NULL,
+    [song]           VARCHAR (500) NULL,
+    [artist]         VARCHAR (500) NULL,
+    [last-week]      VARCHAR (500) NULL,
+    [peak-rank]      VARCHAR (500) NULL,
+    [weeks-on-board] VARCHAR (500) NULL
+)
